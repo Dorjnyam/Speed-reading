be3a6cad125cd786ec227dfd48cafc6c413f4417
Dorjnyam Erdenetsogt <mjldoko11@gmail.com>
2025-12-03 21:13:41 +0800

    fixed

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,11 @@
 import createMiddleware from 'next-intl/middleware';
 import { locales, defaultLocale } from './i18n';
-import { NextRequest } from 'next/server';
 
 export default createMiddleware({
   locales,
   defaultLocale,
   localePrefix: 'as-needed',
-  localeDetection: true,
-  // Use cookie to remember user's explicit locale choice
-  localeCookie: 'NEXT_LOCALE'
+  localeDetection: true
 });
 
 export const config = {
